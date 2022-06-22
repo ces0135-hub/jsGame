42840f245ce9f2fda047fbf77ce248afde69d721
BeakSungHyun <ces0135@naver.com>
2022-06-22 22:20:02 +0900

    Made object for jumping
    Need to make it move

--- avoidObstacle/avoidObstacle.js
+++ avoidObstacle/avoidObstacle.js
@@ -4,32 +4,47 @@
 let canvas = document.querySelector('#myCanvas');
 let ctx = canvas.getContext('2d');
 
-let objHeight = 20;
-let objWidth = 10;
+let canvasHeight = 250;
+let canvasWidth = 1000;
+
+let objHeight = 100;
+let objWidth = 50;
+let objX = 100;
+let objY = canvasHeight - objHeight;
+
+let jumpHeight = 30;
 
 let spaceEntered = false;
 
+//점프할 물체 생성
 function drawObj() {
     ctx.beginPath();
-    ctx.rec(100, 0, objHeight, objWidth);
-    ctx.fillStyle = "black";
+    ctx.rect(objX, objY, objWidth, objHeight);  //rect(x, y, width, height)
+    ctx.fillStyle = "#20752B";
     ctx.fill();
     ctx.close();
 }
 
 function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawObj();
 
-    if(spaceEntered) {
-        alert("Entered");
-    }
+    /*if(spaceEntered) {
+        objY += jumpHeight;
+    }*/
+    objY += jumpHeight;
+    objX += 10;
+
+    //requestAnimationFrame(draw);  //더 깔끔한 애니메이션
 }
 
-let interval = setInterval(draw, 10);
-
-document.addEventListener("spacebar", spacebarHandler, false);
-function spacebarHandler(e) {
-    if(e.keycode == 32) {  //spacebar 입력
+/*
+document.addEventListener('keydown', spacebarOn, false);
+function spacebarOn(e) {
+    if(e.keyCode === 32) {  //spacebar 입력
         spaceEntered = true;
     }
-}
+}*/
+
+setInterval(draw, 10);
+//draw();
