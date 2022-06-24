0a0cffc391a905062197f79500e4abdcf5c34ec5
BeakSungHyun <ces0135@naver.com>
2022-06-24 23:48:44 +0900

    jump 후 원래 착지 완성
    착지하는 과정이 너무 빠른데, 원인을 찾아서 수정해야한다.

--- avoidObstacle/avoidObstacle.js
+++ avoidObstacle/avoidObstacle.js
@@ -4,47 +4,61 @@
 let canvas = document.querySelector('#myCanvas');
 let ctx = canvas.getContext('2d');
 
-let canvasHeight = 250;
-let canvasWidth = 1000;
+let jumpHeight = 20;
+let dy = 5;
 
 let objHeight = 100;
 let objWidth = 50;
-let objX = 100;
-let objY = canvasHeight - objHeight;
+let startX = 100;  //시작 x 좌표
+let startY = canvas.height - objHeight;  //시작 y 좌표
 
-let jumpHeight = 30;
-
-let spaceEntered = false;
+let keyEntered = false;
 
 //점프할 물체 생성
 function drawObj() {
     ctx.beginPath();
-    ctx.rect(objX, objY, objWidth, objHeight);  //rect(x, y, width, height)
+    ctx.rect(startX, startY, objWidth, objHeight);  //rect(x, y, width, height)
     ctx.fillStyle = "#20752B";
     ctx.fill();
-    ctx.close();
+    ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawObj();
 
-    /*if(spaceEntered) {
-        objY += jumpHeight;
-    }*/
-    objY += jumpHeight;
-    objX += 10;
-
-    //requestAnimationFrame(draw);  //더 깔끔한 애니메이션
+    if(keyEntered) {
+        if(startY != jumpHeight) {  //점프
+            startY -= dy;  //상승
+        }
+        if(startY == jumpHeight) {  //최대 높이일 때
+            while(startY != canvas.width - objHeight) {  //바닥에 닿기 전까지
+                startY += dy;  //하강
+                if(startY == canvas.height - objHeight) {  //바닥에 닿을 때
+                   keyEntered = false
+                   break;
+                }
+            }
+        }
+    }
+    
+    requestAnimationFrame(draw);  //더 깔끔한 animation
 }
 
-/*
-document.addEventListener('keydown', spacebarOn, false);
-function spacebarOn(e) {
-    if(e.keyCode === 32) {  //spacebar 입력
-        spaceEntered = true;
-    }
-}*/
 
-setInterval(draw, 10);
-//draw();
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+function keyDownHandler(e) {  //keyUpHandler와 같이 존재해야 한다.
+    if(e.keyCode === 32) {
+        keyEntered = true;
+    }
+}
+function keyUpHandler(e) {  //keyDownHandler와 같이 존재해야 한다.
+    if(e.keyCode === 32) {
+        keyEntered = true;
+    }
+}
+
+
+//setInterval(draw, 1);
+draw();
